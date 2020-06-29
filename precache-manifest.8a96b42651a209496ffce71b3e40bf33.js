811a76411e0fb52e73057422ce27bbf962ae2094
Paul <plai@champ-sys.com>
2020-06-29 10:20:05 +0800

    deploy

---
+++ precache-manifest.8a96b42651a209496ffce71b3e40bf33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69a567ca6c5e7794908cdffaf6da1366",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebf4237912e668284e88",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d69056f0358d2712cd27",
+    "url": "/static/js/2.d498de8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d498de8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebf4237912e668284e88",
+    "url": "/static/js/main.877bc8e3.chunk.js"
+  },
+  {
+    "revision": "98a9bb2308f1d74acf0b",
+    "url": "/static/js/runtime-main.6688e078.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
